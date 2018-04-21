3835e5cd38054730e51e5cf337323d8e8163a39b
MMF <magicmanfoli@gmail.com>
2018-04-21 14:22:44 +0200

    Finding all files, only need to rename now

--- JPG_Giffer.py
+++ JPG_Giffer.py
@@ -1,43 +1,54 @@
 #!python3
 
-import glob
 from pathlib import Path
 from os.path import join
 
-file_extensions = ('*.jpg','*.JPG', '*.jpeg', )
+file_extensions = ('*.jpg', '*.jpeg')   # case insensitive
+
 
 def input_path():
     """encapsulates folder selection"""
-    while True:
-        folder_path = input("Input path to folder:\n>")
+    folder_path = Path(input("Input path to folder:\n>"))
 
-        if folder_path == "":
-            print("Goodbye")
-            exit(0)
+    if folder_path == "":
+        print("Goodbye")
+        exit(0)
+
+    return folder_path
 
 
+def change_extension(file):
+    p = Path(file)
+    file_name = p.stem
+    base_path = p.parent
 
-        if not file_list:  # empty lists are "False"
-            print("no PDFs in that directory, try again")
-        else:
-            break
-    return file_list
+    print("Current: {} in {}".format(file_name, base_path))
+
+
 
 
 def main():
     print("Collecting files")
 
-    folder_path = input_path()
+    # folder_path = input_path()
+    folder_path = Path("E:\Test")
 
     files = []
 
     for ext in file_extensions:
-        files.extend(glob.glob(join(folder_path, ext), recursive=True))
+        mask = "**/" + ext
+        new_files = list(folder_path.glob(mask))
+        print("{} files with {} found".format(len(new_files), ext))
+        files.extend(new_files)
 
-    print("{} files found".format(files.count()))
+    print("{} files found".format(len(files)))
 
+    print("Renaming")
 
+    for file in files:
+        change_extension(file)
 
+    print("Done.")
 
 # end of main
 
